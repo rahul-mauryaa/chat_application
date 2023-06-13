7f73ef7be62b3fa91009117f39ddad22041d8aba
rahul-mauryaa <118436435+rahul-mauryaa@users.noreply.github.com>
2023-06-13 16:20:06 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Register from "./Component/Register";
+import { BrowserRouter ,Routes ,Route } from "react-router-dom";
+const  App = () => {
+  return(
+    <Register />
+
+  )
+  
+}
+
+export default App;
